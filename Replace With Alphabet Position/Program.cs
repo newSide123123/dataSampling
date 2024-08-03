2f9ea494b23399879e32b44c21811cc9fb1ea810
newSide123123 <newSide01901@tutamail.com>
2024-08-03 14:16:48 +0300

    fixed validation bugs

---
+++ Replace With Alphabet Position/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Replace_With_Alphabet_Position
+{
+    internal class Program
+    {
+        public static string AlphabetPosition(string text)
+        {
+            List<char> alphabet = new List<char>();
+
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                alphabet.Add(i);
+            }
+
+            return string.Join(' ', text.ToLower().Where(char.IsLetter).Select(c => alphabet.IndexOf(c) + 1));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AlphabetPosition("\"The sunset sets at twelve o' clock.\""));
+        }
+    }
+}
